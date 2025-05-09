97b7b56ad7ae9cd6c7dbc363c83db6fe3a6449c7
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-09 22:36:53 +0300

    creates anagram class that matches words and returns matched word from a list of words

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        matched_anagrams = []
+        sort_original_word = sorted(self.word)
+
+        for each_word in list:
+            if sorted(each_word) == sort_original_word and each_word.lower() != self.word.lower():
+                matched_anagrams.append(each_word)
+
+
+        return matched_anagrams
+    
+anagram = Anagram("listen")
+print(anagram.match(['enlists', 'google', 'inlets', 'banana', 'netsil']))
+print(anagram.word)
